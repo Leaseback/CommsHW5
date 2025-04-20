5c025b57aa5e71d8b8bc56f9defed1f7f7e8deec
Kyle Kaczorowski <69587926+Leaseback@users.noreply.github.com>
2025-04-20 18:02:28 -0400

    VERSION ATTEMPT 2.1

--- layer3_network_code.py
+++ layer3_network_code.py
@@ -1,5 +1,5 @@
 from mininet.net import Mininet
-from mininet.node import Controller, OVSKernelSwitch, Router
+from mininet.node import Controller, OVSKernelSwitch, Node
 from mininet.topo import Topo
 from mininet.link import TCLink
 from mininet.util import dumpNodeConnections
@@ -7,10 +7,10 @@ from mininet.util import dumpNodeConnections
 
 class MyNetwork(Topo):
     def build(self):
-        # Create routers
-        rA = self.addNode('rA', cls=Router)
-        rB = self.addNode('rB', cls=Router)
-        rC = self.addNode('rC', cls=Router)
+        # Create routers (actually, Hosts acting as routers)
+        rA = self.addNode('rA', cls=Node)
+        rB = self.addNode('rB', cls=Node)
+        rC = self.addNode('rC', cls=Node)
 
         # Create hosts
         hA1 = self.addHost('hA1')
@@ -39,7 +39,30 @@ def runNetwork():
     net.start()
     dumpNodeConnections(net.hosts)
 
-    # Run a simple ping test between hosts on the same LAN
+    # Enable IP forwarding on all routers (hosts acting as routers)
+    rA = net.get('rA')
+    rB = net.get('rB')
+    rC = net.get('rC')
+    rA.cmd('sysctl -w net.ipv4.ip_forward=1')
+    rB.cmd('sysctl -w net.ipv4.ip_forward=1')
+    rC.cmd('sysctl -w net.ipv4.ip_forward=1')
+
+    # Assign IP addresses to the routers and hosts
+    hA1 = net.get('hA1')
+    hA2 = net.get('hA2')
+    hB1 = net.get('hB1')
+    hB2 = net.get('hB2')
+    hC1 = net.get('hC1')
+    hC2 = net.get('hC2')
+
+    hA1.setIP('20.10.172.129/26')
+    hA2.setIP('20.10.172.130/26')
+    hB1.setIP('20.10.172.1/25')
+    hB2.setIP('20.10.172.2/25')
+    hC1.setIP('20.10.172.193/27')
+    hC2.setIP('20.10.172.194/27')
+
+    # Test connectivity within LANs
     print("Testing connectivity within LANs")
     net.pingAll()  # Ping all hosts, expect LAN-only communication.
 
