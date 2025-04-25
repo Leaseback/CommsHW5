a3ef3c807e6ea97117884fca336acf4454b012e1
Kyle Kaczorowski <69587926+Leaseback@users.noreply.github.com>
2025-04-25 12:24:19 -0400

    REVERT

--- layer3_network_code.py
+++ layer3_network_code.py
@@ -5,7 +5,10 @@ from mininet.cli import CLI
 
 
 def start():
-    """Creates a LAN network with 3 routers (A, B, C), each connected to two hosts"""
+    """Creates a LAN network with 3 subnets A, B, and C, and connects 3 routers
+
+    Args:
+    """
 
     net = Mininet(controller=Controller, link=TCLink)
 
@@ -13,32 +16,33 @@ def start():
     net.addController('c0')
 
     # Make the switches
+    # Use s1 s2 s3 since mininet doesn't support sA etc name scheme
     s1 = net.addSwitch('s1')  # LAN A
     s2 = net.addSwitch('s2')  # LAN B
     s3 = net.addSwitch('s3')  # LAN C
 
     # Adding routers
-    r1 = net.addHost('r1')  # Router A
-    r2 = net.addHost('r2')  # Router B
-    r3 = net.addHost('r3')  # Router C
+    r1 = net.addHost('r1')  # Router 1
+    r2 = net.addHost('r2')  # Router 2
+    r3 = net.addHost('r3')  # Router 3
 
     # Hosts
-    hA1 = net.addHost('hA1')
-    hA2 = net.addHost('hA2')
     hB1 = net.addHost('hB1')
     hB2 = net.addHost('hB2')
+    hA1 = net.addHost('hA1')
+    hA2 = net.addHost('hA2')
     hC1 = net.addHost('hC1')
     hC2 = net.addHost('hC2')
 
-    # Connect routers to switches (LANs)
-    net.addLink(r1, s1)  # Router A -> LAN A
-    net.addLink(r1, s2)  # Router A -> LAN B
-    net.addLink(r2, s2)  # Router B -> LAN B
-    net.addLink(r2, s3)  # Router B -> LAN C
-    net.addLink(r3, s3)  # Router C -> LAN C
-    net.addLink(r3, s1)  # Router C -> LAN A
+    # Connect routers to switches
+    net.addLink(r1, s1)  # eth0 -> LAN A
+    net.addLink(r1, s2)  # eth1 -> LAN B
+    net.addLink(r2, s2)  # eth0 -> LAN B
+    net.addLink(r2, s3)  # eth1 -> LAN C
+    net.addLink(r3, s3)  # eth0 -> LAN C
+    net.addLink(r3, s1)  # eth1 -> LAN A
 
-    # Connect hosts to switches (LANs)
+    # Connect hosts to switches
     net.addLink(hA1, s1)
     net.addLink(hA2, s1)
     net.addLink(hB1, s2)
@@ -50,41 +54,41 @@ def start():
     net.start()
 
     print("*** Configuring router interfaces")
-    r1.setIP('20.10.100.1/24', intf='r1-eth0')  # Router A <-> Router B
-    r1.setIP('20.10.100.2/24', intf='r1-eth1')  # Router A <-> Router C
-    r2.setIP('20.10.100.1/24', intf='r2-eth0')  # Router B <-> Router A
-    r2.setIP('20.10.100.3/24', intf='r2-eth1')  # Router B <-> Router C
-    r3.setIP('20.10.100.2/24', intf='r3-eth0')  # Router C <-> Router A
-    r3.setIP('20.10.100.3/24', intf='r3-eth1')  # Router C <-> Router B
+    r1.setIP('20.10.100.1/24', intf='r1-eth0')  # Router 1 eth0 -> LAN A
+    r1.setIP('20.10.100.2/24', intf='r1-eth1')  # Router 1 eth1 -> LAN B
+    r2.setIP('20.10.100.3/24', intf='r2-eth0')  # Router 2 eth0 -> LAN B
+    r2.setIP('20.10.100.4/24', intf='r2-eth1')  # Router 2 eth1 -> LAN C
+    r3.setIP('20.10.100.5/24', intf='r3-eth0')  # Router 3 eth0 -> LAN C
+    r3.setIP('20.10.100.6/24', intf='r3-eth1')  # Router 3 eth1 -> LAN A
 
-    # Configure static routes between routers
-    r1.cmd('ip route add 192.168.2.0/24 via 20.10.100.1')  # Route to LAN B via Router B
-    r1.cmd('ip route add 192.168.3.0/24 via 20.10.100.2')  # Route to LAN C via Router C
+    # Set up routing between routers (static routes)
+    r1.cmd('ip route add 20.10.100.0/24 via 20.10.100.3')  # Route to r2
+    r1.cmd('ip route add 20.10.100.0/24 via 20.10.100.5')  # Route to r3
 
-    r2.cmd('ip route add 192.168.1.0/24 via 20.10.100.1')  # Route to LAN A via Router A
-    r2.cmd('ip route add 192.168.3.0/24 via 20.10.100.3')  # Route to LAN C via Router C
+    r2.cmd('ip route add 20.10.100.0/24 via 20.10.100.1')  # Route to r1
+    r2.cmd('ip route add 20.10.100.0/24 via 20.10.100.5')  # Route to r3
 
-    r3.cmd('ip route add 192.168.1.0/24 via 20.10.100.2')  # Route to LAN A via Router A
-    r3.cmd('ip route add 192.168.2.0/24 via 20.10.100.3')  # Route to LAN B via Router B
+    r3.cmd('ip route add 20.10.100.0/24 via 20.10.100.1')  # Route to r1
+    r3.cmd('ip route add 20.10.100.0/24 via 20.10.100.3')  # Route to r2
 
-    print("*** Configuring hosts IPs")
+    print("*** Configuring hosts")
     # LAN A
-    hA1.setIP('192.168.1.10/24')
-    hA2.setIP('192.168.1.11/24')
-    hA1.cmd('ip route add default via 192.168.1.1')
-    hA2.cmd('ip route add default via 192.168.1.1')
+    hA1.setIP('20.10.100.10/24')
+    hA2.setIP('20.10.100.11/24')
+    hA1.cmd('ip route add default via 20.10.100.1')
+    hA2.cmd('ip route add default via 20.10.100.1')
 
     # LAN B
-    hB1.setIP('192.168.2.10/24')
-    hB2.setIP('192.168.2.11/24')
-    hB1.cmd('ip route add default via 192.168.2.1')
-    hB2.cmd('ip route add default via 192.168.2.1')
+    hB1.setIP('20.10.100.20/24')
+    hB2.setIP('20.10.100.21/24')
+    hB1.cmd('ip route add default via 20.10.100.3')
+    hB2.cmd('ip route add default via 20.10.100.3')
 
     # LAN C
-    hC1.setIP('192.168.3.10/24')
-    hC2.setIP('192.168.3.11/24')
-    hC1.cmd('ip route add default via 192.168.3.1')
-    hC2.cmd('ip route add default via 192.168.3.1')
+    hC1.setIP('20.10.100.30/24')
+    hC2.setIP('20.10.100.31/24')
+    hC1.cmd('ip route add default via 20.10.100.5')
+    hC2.cmd('ip route add default via 20.10.100.5')
 
     print("*** Enabling IP forwarding on routers")
     r1.cmd('sysctl -w net.ipv4.ip_forward=1')
